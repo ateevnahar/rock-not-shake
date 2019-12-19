3c3b3a76d005001a8a354d7836292272ec86ced5
Ateev Nahar <ateevnahar9@gmail.com>
2019-12-19 12:10:33 -0500

    wrap up from work computer

--- rtjvm/src/lectures/part2oop/PackagingAndImports.scala
+++ rtjvm/src/lectures/part2oop/PackagingAndImports.scala
@@ -1,5 +1,10 @@
 package lectures.part2oop
 
+// can import with ._ for everything in the package/class or just by {... , ... } for when you only need
+// a few specific things, ... => ... to do name aliasing -- useful in case of importing more than one
+// class with the same name
+// or you could use fully qualified names
+
 class PackagingAndImports extends App {
 
   // package members are accessible by their simple name
@@ -10,5 +15,12 @@ class PackagingAndImports extends App {
   // packages are in hierarchy
   // matching the folder structure
 
+  sayHello
+  println(meaningOfLife)
+
+  // default imports
+    // java.lang - String, Object, Exception
+    // scala - Int, Nothing, Function
+    // scala.Predef - printLn, ???
 
 }
